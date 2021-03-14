45d6bdea8e34036b79e93e3995d0cafd2f832314
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-03-14 02:34:08 +0000

    functions

---
+++ function_numbers.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_i - print an integer
+ * @print: list of arguments
+ * Return: 
+ */
+int print_i(va_list print)
+{
+}
+/**
+ * print_d - print a decimal
+ * @print: list of arguments
+ * Return: 
+ */
+int print_d(va_list print)
+{
+}
