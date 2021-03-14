b32734d7c3fa7ed1d8a4714da79ba1418869c2b5
SrDri <juanjcarabali@gmail.com>
2021-03-14 03:22:08 +0000

    update %i and %d

--- function_numbers.c
+++ function_numbers.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 #include "holberton.h"
 
 /**
@@ -8,14 +9,38 @@
  * @print: list of arguments
  * Return: 
  */
-int print_i(va_list print)
+int print_i(va_list arg)
 {
-}
-/**
- * print_d - print a decimal
- * @print: list of arguments
- * Return: 
- */
-int print_d(va_list print)
-{
+	int a, i = 0, temp;
+	int factor = 1;
+
+	a = va_arg(arg, int);
+	if (a <= INT_MAX && a >= INT_MIN - 1)
+	{
+		if (a < 0)
+		{
+			a = a * (-1);
+			_putchar('-');
+			i++;
+		}
+		temp = a;
+		i = 0;
+		while (temp)
+		{
+			temp = temp / 10;
+			factor = factor * 10;
+		}
+		while (factor > 1)
+		{
+			factor = factor / 10;
+			_putchar((a / factor) + '0');
+			a = a % factor;
+			i++;
+		}
+		return (i);
+	}
+	else
+	{
+		return (-1);
+	}
 }
