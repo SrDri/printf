6e11183cfda9eb1f34b58d09c1b0ab7f6a9c217f
Juan Jose Carabali <69701234+SrDri@users.noreply.github.com>
2021-03-13 22:16:22 -0500

    Delete "%d"

--- function_numbers.c
+++ function_numbers.c
@@ -4,18 +4,10 @@
 #include "holberton.h"
 
 /**
- * print_i - print an integer
+ * print_i - print an integer and decimal
  * @print: list of arguments
  * Return: 
  */
 int print_i(va_list print)
-{
-}
-/**
- * print_d - print a decimal
- * @print: list of arguments
- * Return: 
- */
-int print_d(va_list print)
 {
 }
