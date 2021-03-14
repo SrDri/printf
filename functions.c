c9f58c623c4f05b86c1dccebf4f11bf041ca3ef1
SrDri <juanjcarabali@gmail.com>
2021-03-14 01:47:42 +0000

    Update print_s

--- functions.c
+++ functions.c
@@ -1,4 +1,8 @@
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
 /**
  * print_c - print a char
  * @print: list of arguments
@@ -11,4 +15,27 @@ int print_c(va_list print)
 	c = (char)va_arg(print, int);
 	_putchar(c);
 	return (1);
+}
+
+/**
+ * _print_string - prints the string, char by char
+ * @print: list of arguments, va_list
+ * Return: the number of printed chars, int
+ */
+int print_s(va_list print)
+{
+	size_t i;
+	char *str = va_arg(print, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	for (i = 0; i < str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (i);
 }
--- holberton.h
+++ holberton.h
@@ -1,4 +1,12 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON H_
 
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_s(va_list print);
+
 #endif /* _HOLBERTON_H_ */
