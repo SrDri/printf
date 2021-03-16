55eaf43db387c34cdb87492a70d05d82a8aea4a1
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-03-16 21:10:52 +0000

    print f commit

--- _printf.c
+++ _printf.c
@@ -7,7 +7,7 @@
  */
 int (*specifier(const char *format))(va_list)
 {
-	unsigned int i;
+	unsigned int i, j;
 
 	_format sformat[] = {
 		{"c", print_c},
@@ -18,9 +18,12 @@ int (*specifier(const char *format))(va_list)
 
 	for (i = 0; sformat[i].n != '\0'; i++)
 	{
-		if (*(sformat[i].n) == *format)
+		for (j = 0; format[j]; j++)
 		{
-			return (sformat[i].func);
+			if (*(sformat[i].n) == format[j])
+			{
+				return (sformat[i].func);
+			}
 		}
 	}
 	return (NULL);
@@ -32,18 +35,21 @@ int (*specifier(const char *format))(va_list)
  */
 int _printf(const char *format, ...)
 {
+	/* while format exist */
 	if (format != NULL)
 	{
 		unsigned int i = 0;
 		unsigned int j = 0;
 
 		va_list _print;
-
+		/* function pointer in the .h file */
 		int (*func)(va_list);
 
 		va_start(_print, format);
+		/*format[i] exist?*/
 		while (format[i])
 		{
+			/* formar != % & != NULL*/
 			while (format[i] != '%' && format[i])
 			{
 				_putchar(format[i]);
@@ -51,24 +57,24 @@ int _printf(const char *format, ...)
 				j++;
 			}
 			if (format[i] == 0)
+				/* if dont exist, return counter*/
 				return (j);
-
+			/*function pointer*/
 			func = specifier(&format[i + 1]);
-
 			if (func != NULL)
 			{
 				j += func(_print);
 				i = i + 2;
+				/* i+2 para que suba en el fomato del main*/
+				/* continue para que cambie de posición*/
 				continue;
 			}
-
-			if (!format[i + 1])
+			/* si format no contiene %*/
+			if (format[i + 1] == 0)
 				return (-1);
 
 			_putchar(format[i]);
-
 			j++;
-
 			if (format[i + 1] == '%')
 				i = i + 2;
 			else
